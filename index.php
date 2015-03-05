922de1c19dcc5fb386323eaed3eb9ae8c4fc72fe
Rameez_Iqbal <Rameez_Iqbal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-05 06:35:12 +0000

    Image Caption Hover 3.1
    git-svn-id: https://plugins.svn.wordpress.org/image-caption-hover/tags/3.1@1105743 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Image Caption Hover
+ * Plugin URI: http://webcodingplace.com/image-caption-hover/
+ * Description: A simple way to add responsive images in widgets with caption.
+ * Version: 3.1
+ * Author: Rameez
+ * Author URI: http://webcodingplace.com/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: image-caption-hover
+ */
+
+/*
+
+  Copyright (C) 2015  Rameez  rameez.iqbal@live.com
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+*/
+require_once('plugin.class.php');
+
+?>
---
+++ js/script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+	
+	setTimeout(function() {
+		$('.wcp-caption-image').each(function() {
+			var current_width = $(this).width();
+			var current_height = $(this).height();
+			var current_wraper = '#' + $(this).closest('.wcp-caption-plugin').attr('id');
+			$(current_wraper).find('.image-caption-box, .caption').css({
+				'width': current_width,
+				'height': current_height
+			});
+		});	
+	}, 20);
+});
